e26b0bdca5078d09611f9db944e228e2bb8e82ba
toonyrafael <toonyrafael@gmail.com>
2025-07-01 00:39:31 -0300

    Adicionado Pedido do Issue de Teste

--- src/Projeto/Contador.java
+++ src/Projeto/Contador.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Contador {
 	public static void main(String[] args) {
 		Scanner terminal = new Scanner(System.in);
+		System.out.println("Hello World");
 		System.out.println("Digite o primeiro parâmetro");
 		int parametroUm = terminal.nextInt();
 		System.out.println("Digite o segundo parâmetro");
